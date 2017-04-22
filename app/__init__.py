537b9ababf981cdae74828139f1f21748c6673d1
csdt <csdt@csdt.fr>
2017-04-23 00:33:41 +0200

    Added migrations

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 ### config
 app = Flask("Pawi")
@@ -13,6 +15,11 @@ app.config.from_pyfile("config.py")
 db = SQLAlchemy(app)
 admin = Admin()
 admin.init_app(app)
+
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 ### Internal imports
 from .controllers import *
--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # -*-coding:utf-8 -*-
 
-from app import app
+from app import manager
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    manager.run()
